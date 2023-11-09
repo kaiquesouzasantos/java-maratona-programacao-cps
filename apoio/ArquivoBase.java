28f4655ba7feda42828b184e999a45d9e9a1e3b0
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2023-11-09 15:00:00 -0300

    Update ArquivoBase.java

--- apoio/ArquivoBase.java
+++ apoio/ArquivoBase.java
@@ -1,16 +1,25 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class ArquivoBase {
-	private static Scanner terminal = new Scanner(System.in);
+    private static final Scanner terminal = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		tratamento();
-		terminal.close();
-	}
+    public static void main(String[] args) {
+        tratamento();
+        terminal.close();
+    }
 
-	private static void tratamento() {
-		try {
-
+    private static void tratamento() {
+        try {
+            while(true) {
+                int leitura = terminal.nextInt();
+                encerramento(leitura);
+            }
         } catch(Exception ignored) {}
-	}
+    }
+
+    private static void encerramento(int leitura) {
+        if(leitura == 0) {
+            System.exit(0);
+        }
+    }
 }
