f20dcc1fa82d09b594cca0ae5562d74cbe280453
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2023-11-09 14:24:44 -0300

    Add files via upload

---
+++ apoio/ArquivoBase.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ArquivoBase {
+	private static Scanner terminal = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		tratamento();
+		terminal.close();
+	}
+
+	private static void tratamento() {
+		try {
+
+        } catch(Exception ignored) {}
+	}
+}
